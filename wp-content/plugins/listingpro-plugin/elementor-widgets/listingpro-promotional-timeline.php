1af0b2f331053061aac95bdd066e10019e6c5c0c
eujinong <eujinong1@gmail.com>
2020-05-19 12:34:46 +0300

    real-estate-project

---
+++ wp-content/plugins/listingpro-plugin/elementor-widgets/listingpro-promotional-timeline.php
@@ -0,0 +1,159 @@
+<?php
+namespace ElementorListingpro\Widgets;
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+class Listingpro_Promotional_Timeline extends Widget_Base {
+
+    public function get_name() {
+        return 'listingpro-promotional-timeline';
+    }
+
+    public function get_title() {
+        return __( 'Promotional Timeline', 'elementor-listingpro' );
+    }
+
+    public function get_icon() {
+        return 'eicon-posts-ticker';
+    }
+
+    public function get_categories() {
+        return [ 'listingpro' ];
+    }
+    protected function _register_controls() {
+        $this->start_controls_section(
+            'section_content',
+            [
+                'label' => __( 'Content', 'elementor-listingpro' ),
+            ]
+        );
+        $this->add_control(
+            'listing_pro_timeline_title',
+            [
+                'label' => __( 'Timeline Title', 'elementor-listingpro' ),
+                'type' => Controls_Manager::TEXT,
+            ]
+        );
+        $this->add_control(
+            'pro_timeline_short_desc',
+            [
+                'label' => __( 'Timeline Short Description', 'elementor-listingpro' ),
+                'type' => Controls_Manager::TEXTAREA,
+            ]
+        );
+        $this->add_control(
+            'pro_timeline_title_first',
+            [
+                'label' => __( 'Timeline First Title', 'elementor-listingpro' ),
+                'type' => Controls_Manager::TEXT,
+            ]
+        );
+        $this->add_control(
+            'pro_timeline_desc_first',
+            [
+                'label' => __( 'Timeline Short Description', 'elementor-listingpro' ),
+                'type' => Controls_Manager::TEXTAREA,
+            ]
+        );
+        $this->add_control(
+            'pro_timeline_first_img',
+            [
+                'label' => __( 'Timeline Right Image', 'elementor-listingpro' ),
+                'type' => \Elementor\Controls_Manager::MEDIA,
+                'default' => [ 'url' => get_template_directory_uri()."/assets/images/time1.png" ]
+            ]
+        );
+        $this->add_control(
+            'pro_timeline_title_second',
+            [
+                'label' => __( 'Timeline Second Title', 'elementor-listingpro' ),
+                'type' => Controls_Manager::TEXT,
+            ]
+        );
+        $this->add_control(
+            'pro_timeline_desc_second',
+            [
+                'label' => __( 'Timeline Second Description', 'elementor-listingpro' ),
+                'type' => Controls_Manager::TEXTAREA,
+            ]
+        );
+        $this->add_control(
+            'pro_timeline_second_img',
+            [
+                'label' => __( 'Timeline Left Image', 'elementor-listingpro' ),
+                'type' => \Elementor\Controls_Manager::MEDIA,
+                'default' => [ 'url' => get_template_directory_uri()."/assets/images/time2.png" ]
+            ]
+        );
+
+        $this->end_controls_section();
+
+        $this->start_controls_section(
+            'section_style',
+            [
+                'label' => __( 'Style', 'elementor-listingpro' ),
+                'tab' => Controls_Manager::TAB_STYLE,
+            ]
+        );
+        $this->add_control(
+            'text_transform',
+            [
+                'label' => __( 'Text Transform', 'elementor-listingpro' ),
+                'type' => Controls_Manager::SELECT,
+                'options' => [
+                    '' => __( 'None', 'elementor-hello-world' ),
+                    'uppercase' => __( 'UPPERCASE', 'elementor-listingpro' ),
+                    'lowercase' => __( 'lowercase', 'elementor-listingpro' ),
+                    'capitalize' => __( 'Capitalize', 'elementor-listingpro' ),
+                ],
+                'selectors' => [
+                    '{{WRAPPER}} .title' => 'text-transform: {{VALUE}};',
+                ],
+            ]
+        );
+        $this->end_controls_section();
+    }
+    protected function render() {
+        $settings = $this->get_settings_for_display();
+        echo listingpro_shortcode_pro_services( $settings );
+    }
+    protected function content_template() {}
+    public function render_plain_content() {}
+}
+if(!function_exists('listingpro_shortcode_pro_services')) {
+    function listingpro_shortcode_pro_services($atts, $content = null) {
+        extract(shortcode_atts(array(
+            'listing_pro_services_img'      => get_template_directory_uri()."/assets/images/servcs1.png",
+            'listing_pro_services_title'         => '',
+            'pro_services_desc' 					=> '',
+        ), $atts));
+
+        $output = null;
+
+        $thumbImg = '';
+        if (!empty($listing_pro_services_img)) {
+            if( is_array( $listing_pro_services_img ) )
+            {
+                $listing_pro_services_img   =   $listing_pro_services_img['id'];
+            }
+            $bgImage = wp_get_attachment_image_src( $listing_pro_services_img, 'full' );
+            $thumbImg = '<img src="'.$bgImage[0].'" alt="">';
+        }else{
+            $thumbImg = '';
+        }
+
+        $output .='
+	<div class="promotional-service">
+		<div class="promotiona-thumb">
+			'.$thumbImg.'
+		</div>
+		<div class="promotiona-text-details">
+			<h3>'.$listing_pro_services_title.'</h3>
+			<p>'.$pro_services_desc.'</p>
+		</div>
+	</div>';
+
+        return $output;
+    }
+}
